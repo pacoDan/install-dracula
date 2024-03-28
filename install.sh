cb4cc5a3ce263187729a530eaaae4c36ecb69d30
pacoDan <jhonpaco@frba.utn.edu.ar>
2024-03-27 22:57:16 -0300

    :tada:

---
+++ install.sh
@@ -0,0 +1,8 @@
+cp -rf ./theme-gtk-dracula ~/.themes/
+sudo cp -rf ./theme-gtk-dracula /usr/share/themes/
+echo "-> installed GTK"
+
+mkdir -p ~/.config/tilix/schemes && cp -f ./tilix/Dracula.json ~/.config/tilix/schemes/ && echo "Installer Tilix"
+echo "-> installed Tilix"
+echo "----------------------->"
+echo "Done!"
